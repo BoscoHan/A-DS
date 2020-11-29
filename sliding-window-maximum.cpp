baf1a9381c609989936eb511e97e464d53e2fe0c
Bosco Han <yunzhi98han@gmail.com>
2020-11-29 00:05:37 -0700

    working commit - Created using LeetHub

---
+++ sliding-window-maximum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        vector<int> result;
+        deque<int> dq;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            while (!dq.empty() && i-k >= dq.front())
+                dq.pop_front();
+            
+            while (!dq.empty() && nums[dq.back()] < nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            if (i >= k-1)
+                result.push_back(nums[dq.front()]);
+        }
+        
+        return result;
+    }
+};

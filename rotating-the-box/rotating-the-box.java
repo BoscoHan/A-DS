4d3518d4fcfe190170bc92d4b129b7226d3474cd
Bosco Han <yunzhi98han@gmail.com>
2021-11-08 22:41:19 -0700

    Time: 18 ms (22.30%), Space: 75.4 MB (70.55%) - LeetHub

--- rotating-the-box/rotating-the-box.java
+++ rotating-the-box/rotating-the-box.java
@@ -4,35 +4,35 @@ class Solution {
         int m = box.length, n = box[0].length;
         char[][] Rbox = new char[n][m];
         
-        //move stones to right most
-        for (char[] row : box) {
-            for (int j = n-1; j >=0; j--) {
-                if (row[j] == '#') {
-                    
-                    int curCol = j;
-                    
-                    //while not at end and spot empty:
-                    while (curCol + 1 < n && row[curCol+1] == '.') 
-                        curCol++;
-                    
-
-                    //if shifted stones to the right
-                    if (curCol != j) {
-                        row[curCol] = '#';
-                        row[j] = '.';
-                    }
-                }
-            }
-        }
         
-        // System.out.println(Arrays.deepToString(box));
-        
-        //rotate:
+        //rotate box:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 Rbox[j][i] = box[m-i-1][j];
             }
         }
+        
+        // System.out.println(Arrays.deepToString(Rbox));
+        
+        
+        for (int j = 0; j < Rbox[0].length; j++) {
+            for (int i = Rbox.length-1; i >=0; i--) {
+                if (Rbox[i][j] == '#') {
+                    int currRow = i;
+                    
+                    while (currRow+1 < Rbox.length && Rbox[currRow+1][j] == '.') 
+                        currRow++;
+                    
+                    
+                    if (currRow != i) {
+                        //swap stone w empty space
+                        Rbox[currRow][j] = '#';
+                        Rbox[i][j] = '.';
+                    }                    
+                }
+            }       
+        }
+        
         
         return Rbox;
     }

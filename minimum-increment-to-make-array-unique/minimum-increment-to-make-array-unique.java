18e7f049eb8d6078ef1bdb2ae8a9181309d52d10
Bosco Han <yunzhi98han@gmail.com>
2021-11-08 20:31:38 -0700

    Time: 36 ms (49.77%), Space: 55.9 MB (48.72%) - LeetHub

--- minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -1,42 +1,25 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-        if (nums.length == 0)
+        if (nums.length <= 1)
             return 0;
-        
-        int moves = 0;
-        
-        // 3 2 1 2 1 7
-        
-        
-        // 1 1 2 2 3 7 nums
-        // 1 2 3 4 5 
-        
-        //moves 6
-        Arrays.sort(nums);
-        
-//         int valToLookFor = nums[0] + 1;
-        
-//         for (int i = 1; i < nums.length; i++) {
-//             if (nums[i] > nums[i-1]) {
-//                 moves += 
-                
-//             }
             
-//         }
+        Arrays.sort(nums);
+        int res = 0;
+        int valToLook = nums[0] + 1;
         
+        //[3,2,1,2,1,7]
+        //[1,1,2,2,3,7]
         
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]) {
-                moves++;
-                nums[i]++;
-
-            } else if (nums[i] < nums[i-1]) {
-             
-                moves +=  nums[i-1] - nums[i] + 1;
-                nums[i]= nums[i-1] + 1;
-            }            
+            
+            if (nums[i] > nums[i-1]) 
+                valToLook = Math.max(valToLook, nums[i]);
+            
+            
+            res += valToLook - nums[i];
+            valToLook++;
         }
-                
-        return moves;
+        
+        return res;
     }
 }

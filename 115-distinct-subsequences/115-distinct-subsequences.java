2ef061bce088f0744db3ae5291a1197f71a3a7fa
Bosco Han <yunzhi98han@gmail.com>
2022-03-20 19:57:40 -0400

    Time: 20 ms (72.86%), Space: 49.1 MB (75.31%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        if(t.length() == 0) return 1;
+        if(s.length() == 0) return 0;
+        
+        int[][] memo = new int[s.length()][t.length()];
+        Arrays.stream(memo).forEach(row -> Arrays.fill(row, -1));
+
+        //bagbag , ba
+        return countSubSequence(s, t, s.length(), t.length(), memo);
+    }
+    
+    private int countSubSequence(String s, String t, int s_idx, int t_idx, int[][] memo) {
+        if(t_idx == 0) return 1;
+        if(s_idx == 0) return 0;
+        
+        int ans = memo[s_idx-1][t_idx-1];
+        if (ans != -1)
+            return ans;
+        
+        //if current letter from back matches:
+        if (s.charAt(s_idx-1) == t.charAt(t_idx-1)) {
+            //next itr can be any 2 combos, go back 1 letter on both, or go back one letter on longer str
+            ans = countSubSequence(s, t, s_idx-1, t_idx-1, memo) + countSubSequence(s, t, s_idx-1, t_idx, memo);
+        } else {
+            //keep checking the longer string to see if match is earlier in string
+            ans = countSubSequence(s, t, s_idx-1, t_idx, memo);
+        }
+        
+        return memo[s_idx-1][t_idx-1] = ans;
+    }
+}

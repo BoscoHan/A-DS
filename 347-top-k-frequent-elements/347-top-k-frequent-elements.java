e89f7008a56959ff5378157b63a03b10dbc59545
Bosco Han <yunzhi98han@gmail.com>
2022-04-09 18:23:11 -0400

    Time: 8 ms (97.78%), Space: 44.7 MB (90.63%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
         Map<Integer, Integer> map = new HashMap();
         for (int n : nums) 
             map.put(n, map.getOrDefault(n, 0) + 1);
         
-        PriorityQueue<Integer> pq = new PriorityQueue((a, b) -> map.get(a) - map.get(b));
+        //bucket sort:
+        // n + 1 buckets, the list in buckets[i] contains elements with the same frequency i
+        var bucket = new List[nums.length + 1];
+        
         for (int key : map.keySet()) {
-            pq.offer(key);
-            
-            while (pq.size() > k) {
-                pq.poll();
+            int freq = map.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList();
             }
+            
+            bucket[freq].add(key);    
         }
         
         int[] res = new int[k];
-        for (int i = 0; i < k; i++) 
-            res[i] = pq.poll();
+        int res_index = 0;
+        for (int j = bucket.length-1; j >= 0; j--) {
+            if (bucket[j] != null) {
+                if (res_index >= k)
+                    break;
+                
+                for (int i = 0; i < bucket[j].size(); i++) {
+                    res[res_index++] = (int) bucket[j].get(i);
+                }
+            }
+        }
         
         return res;
     }
